fb5b137fabf8cd7c7ae303528af66febf99ea3cb
William Yurkow <williamy@tlcengine.com>
2021-01-18 18:01:19 -0500

    I added react-bootstrap, added some more styling to header, footer, added containers to both, added font awesome for some icons on the navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To Memory Lane Vintage</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Memory Lane Vintage</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
