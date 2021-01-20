0cb868878ba8436d8fb3bcfa174dd1bf0f505491
William Yurkow <williamy@tlcengine.com>
2021-01-20 18:43:07 -0500

    created the home page and displayed images of the furniture being displayed for sale

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Vintage Emerald Green Sofa',
+    image: '/images/vintageemeraldgreensofa.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1049.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Mid Century Modern Leather Sofa',
+    image: '/images/vintageleathersofa.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Article',
+    category: 'Furniture',
+    price: 1599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Mid Century Modern Chair',
+    image: '/images/midcenturymodernchair.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Mid Mod',
+    category: 'Furniture',
+    price: 74.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Mid Century Modern High Chair',
+    image: '/images/midcenturymodernhighchair.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Mid Mod',
+    category: 'Furniture',
+    price: 99.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Danish Teak Armoire',
+    image: '/images/danishteakarmoire.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Mid Mod',
+    category: 'Furniture',
+    price: 1549.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Danish Teak Chair',
+    image: '/images/danishteakchair.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Mid Mod',
+    category: 'Furniture',
+    price: 349.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
