b7548d46f5dcb2855158500f6dc2ae5a02e7c7be
William Yurkow <williamy@tlcengine.com>
2021-02-01 16:42:19 -0500

    I connected the frontend and the backend. I installed axios for http requests and am now able to display the proudcts from the backend onto the dom.

--- frontend/src/Pages/ProductPage.js
+++ frontend/src/Pages/ProductPage.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import {
-  Row,
-  Col,
-  Image,
-  ListGroup,
-  Card,
-  Button,
-  ListGroupItem,
-} from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Ratings'
-import products from '../products'
+import axios from 'axios'
 
 const ProductPage = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
-  console.log(product)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
 
   return (
     <>
